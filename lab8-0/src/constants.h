871902583dd31215bfcc78a4e5414fd4d03490fb
quiet_arbor <110762381+QuietGerbi@users.noreply.github.com>
2025-03-31 16:55:14 +0700

    Delete lab8-0 directory

--- lab8-0/src/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-typedef struct {
-    int u, v, weight;
-} Edge;
-
-#endif
--- lab8-0/src/kruskal.h
+++
@@ -1,9 +0,0 @@
-#ifndef KRUSKAl_H
-#define KRUSKAl_H
-
-#include "constants.h"
-
-int kruskal(int n, int edge_count, Edge edges[], Edge saved_edges[]);
-
-#endif
-
